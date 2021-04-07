592cdae61ca8e08158884dcde29ac6c2ba60de8f
BernardaP <btp210@gmail.com>
2021-04-06 23:10:56 -0600

    adds library route

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.get('/profile', isLoggedIn, (req, res) => {
 
 app.use('/auth', require('./routes/auth'));
 app.use('/home', require('./routes/home'));
+app.use('/library', require('./routes/library'));
 
 var server = app.listen(process.env.PORT || 3000, ()=> console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
 
