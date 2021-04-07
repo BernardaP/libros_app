b0f83b55df094a26cf15c57bed3fcbee74dfa0f3
BernardaP <btp210@gmail.com>
2021-04-06 21:40:23 -0600

    creates migrations for all models

---
+++ migrations/20210406170220-create-book.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.FLOAT
+      },
+      ebook: {
+        type: Sequelize.BOOLEAN
+      },
+      buy: {
+        type: Sequelize.STRING
+      },
+      isbn: {
+        type: Sequelize.STRING
+      },
+      pageCount: {
+        type: Sequelize.INTEGER
+      },
+      publisher: {
+        type: Sequelize.STRING
+      },
+      publishedDate: {
+        type: Sequelize.STRING
+      },
+      authorId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('books');
+  }
+};
