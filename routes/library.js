d9390bc2fa42b9bdfafb5c3a97fca4e3bd45c005
BernardaP <btp210@gmail.com>
2021-04-07 00:35:35 -0600

    creates the library route where all the existing books are retrieved from the database and also creates a post route to save the user book to the library

--- routes/library.js
+++ routes/library.js
@@ -5,12 +5,36 @@ const axios = require('axios')
 
 //Get - /library show all boooks to the user
 router.get('/', function(req, res) {
-  res.send('library page')
+  //find all the books in db
+  db.book.findAll()
+    .then((foundBooks) => {
+      console.log("+++++++",foundBooks)
+       //render the view and pass the found books
+      res.render('library/index.ejs', {books: foundBooks})
+  })
+  
+ 
+ 
 })
 
 //POST /library save a book to the library
 router.post('/', function(req, res) {
-  res.redirect('/library')
+  //check the contents of body
+  // console.log(req.body)
+  //call database and add a book via find or create
+  db.book.findOrCreate({
+    where: {
+      isbn: req.body.isbn,
+      
+    },
+    defaults: {
+      title: req.body.title
+    }
+
+  }).then(([book, created]) => {
+    res.redirect('/library')
+
+  })
 })
 
 
