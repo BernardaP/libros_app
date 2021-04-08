79b71898013015408e268df1ec3ac7feac8213e4
BernardaP <btp210@gmail.com>
2021-04-07 23:34:17 -0600

    creates skeleton for book review post form

--- routes/book.js
+++ routes/book.js
@@ -1,6 +1,20 @@
 const router = require('express').Router()
 const passport = require('../config/ppConfig')
 const db = require("../models")
-const axios = require('axios')
 const { route } = require('./home')
 
+
+//POST - / to create a review
+// router.post('/', function(req, res) {
+//   //find all the books in db
+//   db.book.findOrCreate()
+//     .then((foundBooks) => {
+//       // console.log("+++++++",foundBooks)
+//        //render the view and pass the found books
+//       res.render('library/index.ejs', {books: foundBooks})
+//   })
+ 
+// })
+
+
+module.exports = router;
