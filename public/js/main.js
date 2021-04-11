e70f1d09107b9486516427eb03cfb5a485c9792c
BernardaP <btp210@gmail.com>
2021-04-11 00:28:19 -0600

    adds script for date picker and alerts

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
+M.AutoInit();
+
+// Date Picker
+
+const dateEl = document.getElementById('id_date');
+M.Datepicker.init(dateEl, {
+  format: 'yyyy-mm-dd',
+  defaultDate: new Date(),
+  setDefaultDate: true,
+  autoClose: false
 });
 
-M.AutoInit(); 
+// Alerts
+const close = document.getElementsByClassName("closebtn");
+let i;
+
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function(){
+    let div = this.parentElement;
+    div.style.opacity = "0";
+    setTimeout(function(){ div.style.display = "none"; }, 600);
+  }
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.sidenav');
+//   var instances = M.Sidenav.init(elems, options);
+// });
+
+// M.AutoInit(); 
 
 
