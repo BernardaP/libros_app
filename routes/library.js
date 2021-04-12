8cfcf464a39841c2f4374726159605a6d9a1b5d8
BernardaP <btp210@gmail.com>
2021-04-11 20:00:33 -0600

    cleans and refactors routes

--- routes/library.js
+++ routes/library.js
@@ -6,9 +6,6 @@ const { route } = require('./home')
 
 //Get - /library show all books added to library 
 router.get('/', function(req, res) {
-  // console.log("hello")
-  //find all the books in db
-  console.log("HERE>>>>>>", req.user)
   db.user.findOne({
     where: {
       id: req.user.id
@@ -17,47 +14,16 @@ router.get('/', function(req, res) {
       model: db.book, include: [db.author]
     } 
   }).then((user)=> {
-    console.log("*******", user.books)
     res.render('library/index.ejs', { user: user })
   })
-
-  
-
-
-
-  // db.book.findAll({
-  //   include: [db.author]
-  // })
-  //   .then((foundBooks) => {
-  //     db.author.findAll()
-  //     .then((authors) => {
-  //       db.user.findOne({
-  //         where: {
-  //           id: req.user.id
-  //         }
-  //     }).then((user) => {
-  //       res.render('library/index.ejs', {books: foundBooks, author: authors, user: user })
-
-  //     })
-  //   })
-  //     //console.log("+++++++",foundBooks)
-  //     // render the view and pass the found books
-  // })
- 
+  .catch(function(error){
+    req.flash('error', error.message)
+    res.send('Error!')
+  })
 })
 
 //GET -/library/:id
 //show detail info of a book
-// router.get('/:id', function(req, res) {
-//   db.book.findByPk(req.params.id)
-//   .then((foundBook)=> {
-//     res.render('library/show.ejs', { book: foundBook})
-//   })
-//   .catch(function(error){
-//     console.log(error)
-//     res.send('Error!')
-//   })
-// })
 router.get('/:id', function(req, res) {
   db.book.findOne({
     where: {
@@ -71,7 +37,7 @@ router.get('/:id', function(req, res) {
     res.render('library/show.ejs', { book: foundBook})
   })
   .catch(function(error){
-    console.log(error)
+    req.flash('error', error.message)
     res.send('Error!')
   })
 })
@@ -99,57 +65,11 @@ router.post('/', async function(req, res) {
     res.redirect('/library')
     
   } catch (error) {
-    console.log(error)
+    req.flash('error', error.message)
     res.send('Error ;(!')
   }
 })
 
-// router.post('/', function(req, res) {
-//   //check the contents of body
-//   // console.log(req.body)
-//   //call database and add a book via find or create
-//   db.book.findOrCreate({
-//     where: {
-//       title: req.body.title,
-//       image: req.body.image,
-//       description: req.body.description
-//     },
-//     defaults: {
-//       isbn: req.body.isbn
-//     }
-
-//   }).then(function ([book, created]) {
-//     // console.log("%%%%%%%%%%%%%",book)
-//     db.author.findOrCreate({
-//       where: {
-//         firstName: req.body.author
-//       }
-//     }).then(function([author, created]) {
-//       console.log("%%%%%%%%%%%%%",author)
-//       author.addBook(book).then(function(relationInfo){ 
-        
-//         db.user.findOne({
-//           where: {
-//             id: req.user.id
-//           }
-//         }).then((user) => {
-//           user.addBook(book).then(function(relationInfo){
-//             res.redirect('/library')
-//           })
-//         })  
-//       // book.addAuthor(author.id).then(function(relationInfo){ 
-//         // console.log('===========AUTHOR', author.firstName, "added to ", book.title)
-//       })
-
-//     })
-//     // res.redirect('/library')
-
-//   })
-//   .catch(function(error){
-//     console.log(error)
-//     res.send('Error ;(!')
-//   })
-// })
 
 
 // POST - delete a book from library
